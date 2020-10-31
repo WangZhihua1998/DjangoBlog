01804573c0cd056bf5ae70c490beccca36e10246
Wang Zhihua <1069155322@qq.com>
2020-10-31 09:15:18 +0800

    将数据库换为sqlite3

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -98,19 +98,25 @@ WSGI_APPLICATION = 'DjangoBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DJANGO_MYSQL_DATABASE') or 'djangoblog',
-        'USER': os.environ.get('DJANGO_MYSQL_USER') or 'root',
-        'PASSWORD': os.environ.get('DJANGO_MYSQL_PASSWORD') or 'djangoblog_123',
-        'HOST': os.environ.get('DJANGO_MYSQL_HOST') or '127.0.0.1',
-        'PORT': int(
-            os.environ.get('DJANGO_MYSQL_PORT') or 3306),
-        'OPTIONS': {
-            'charset': 'utf8mb4'},
-    }}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': os.environ.get('DJANGO_MYSQL_DATABASE') or 'djangoblog',
+#        'USER': os.environ.get('DJANGO_MYSQL_USER') or 'root',
+#        'PASSWORD': os.environ.get('DJANGO_MYSQL_PASSWORD') or 'djangoblog_123',
+#        'HOST': os.environ.get('DJANGO_MYSQL_HOST') or '127.0.0.1',
+#        'PORT': int(
+#            os.environ.get('DJANGO_MYSQL_PORT') or 3306),
+#        'OPTIONS': {
+#            'charset': 'utf8mb4'},
+#    }}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
